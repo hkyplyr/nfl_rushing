f82fc9093a4a8b15176d896434a84100404201c8
Travis Paquette <tpaqu15@gmail.com>
2021-05-17 20:54:04 -0600

    Re-organize app to help with testing

--- nfl_rushing/app.py
+++ nfl_rushing/app.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
 from nfl_rushing.database import db
+from nfl_rushing.views import bp
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rushing.db'
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db.init_app(app)
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rushing.db'
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    app.register_blueprint(bp)
+    return app
