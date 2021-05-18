bfcabe43478cf94aa06977e79a6e1e2517a49995
Travis Paquette <tpaqu15@gmail.com>
2021-05-17 20:54:04 -0600

    Re-organize app to help with testing

--- nfl_rushing/views.py
+++ nfl_rushing/views.py
@@ -1,10 +1,11 @@
 import csv
-from flask import render_template, request, send_file, url_for
-from nfl_rushing.app import app
+from flask import Blueprint, render_template, request, send_file, url_for
 from nfl_rushing.models import Player
 
+bp = Blueprint('bp', __name__)
 
-@app.route('/')
+
+@bp.route('/')
 def index():
     sort = __get_sort_query_param()
     page = __get_page_query_param()
@@ -12,17 +13,17 @@ def index():
 
     players = __get_base_query(name, sort).paginate(page, 15)
 
-    sort_td_url = url_for('app.index', page=page, sort='td', name=name)
-    sort_yds_url = url_for('app.index', page=page, sort='yds', name=name)
-    sort_lng_url = url_for('app.index', page=page, sort='lng', name=name)
-    download_url = url_for('app.download', sort=sort, name=name)
-    next_url = url_for('app.index', page=players.next_num, sort=sort) if players.has_next else None
-    prev_url = url_for('app.index', page=players.prev_num, sort=sort) if players.has_prev else None
+    sort_td_url = url_for('bp.index', page=page, sort='td', name=name)
+    sort_yds_url = url_for('bp.index', page=page, sort='yds', name=name)
+    sort_lng_url = url_for('bp.index', page=page, sort='lng', name=name)
+    download_url = url_for('bp.download', sort=sort, name=name)
+    next_url = url_for('bp.index', page=players.next_num, sort=sort) if players.has_next else None
+    prev_url = url_for('bp.index', page=players.prev_num, sort=sort) if players.has_prev else None
     return render_template('index.html', players=players.items, prev_url=prev_url, next_url=next_url, sort_td_url=sort_td_url,
                            sort_yds_url=sort_yds_url, sort_lng_url=sort_lng_url, download_url=download_url)
 
 
-@app.route('/download')
+@bp.route('/download')
 def download():
     sort = __get_sort_query_param()
     name = __get_name_query_param()
@@ -37,7 +38,7 @@ def download():
         for player in players:
             writer.writerow([getattr(player, c) for c in headers])
 
-    return send_file(filename, attachment_filename=filename, as_attachment=True, cache_timeout=-1)
+    return send_file(filename, download_name=filename, as_attachment=True, max_age=-1)
 
 
 def __get_sort_query_param():
