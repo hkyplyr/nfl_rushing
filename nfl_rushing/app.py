ebb6120fed480486dd5b1c5e764172d298858806
Travis Paquette <tpaqu15@gmail.com>
2021-05-19 01:04:17 -0600

    Add proper documentation
    - Add docstrings
    - Rename or refactor methods to be more explicit in what they do

--- nfl_rushing/app.py
+++ nfl_rushing/app.py
@@ -8,21 +8,36 @@ from nfl_rushing.models import Player
 
 
 def create_app():
+    """ Create the flask application
+
+    This method is responsible for creating the root flask
+    app object, configuring the use of an in-memory sqlite
+    database, registering the blueprints (routes), and lastly
+    initializing and loading the data into the database.
+
+    Since this app uses an in-memory database the schema can
+    be created and the data can be loaded on startup without
+    issue.
+    """
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.register_blueprint(bp)
 
-    db.init_app(app)
-    app.app_context().push()
-    db.create_all()
-    load_json_data()
+    __initialize_db(app)
+    __load_json_data()
     return app
 
 
-def load_json_data():
+def __initialize_db(app):
+    db.init_app(app)
+    app.app_context().push()
+    db.create_all()
+
+
+def __load_json_data():
     with open('rushing.json', 'r') as f:
         rushing_data = json.load(f)
-    players = [Player.init(d) for d in rushing_data]
+    players = [Player.to_database_model(d) for d in rushing_data]
     db.session.add_all(players)
     db.session.commit()
